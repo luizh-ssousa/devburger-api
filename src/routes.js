bdc613730b4fc1eee8d9899a343ca72a37c8c839
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-02 11:30:26 -0300

    nodemon e sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+import { Router } from 'express'
 
 const routes = new Router()
 
@@ -6,4 +6,4 @@ routes.get('/', (request, response) => {
     return response.status(200).json({ message: 'Hello, World!' })
 })
 
-module.exports = routes
+export default routes
