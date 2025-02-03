20dfc509a4010648e59a626b6d8b5ddcda334699
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-02 21:53:20 -0300

    userController

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
-import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({ 
-        name: 'John Doe', 
-        email: 'johndoe@example.com',
-        password_hash: 'sldkfjsoij8894jd',
-    });
-
-    return response.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
