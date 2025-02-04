07d7425760f2ae1d3ebe745e5cba9b88a6fc6f75
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-04 20:23:11 -0300

    controller

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SesssionController from './app/controllers/SesssionController';
-
+import ProductController from './app/controllers/ProductController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SesssionController.store);
+routes.post('/products', ProductController.store);
 
 export default routes;
