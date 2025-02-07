9c461ef2ae5c6c5b3d16fca012e6f442d7d73cc3
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-06 21:34:41 -0300

    scheme de pedidos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,12 +1,14 @@
 import * as Yup from 'yup';
 import Product from '../models/Product';
+import Category from '../models/Category';
+
 
 class ProductController {
   async store(request, response) {
     const schema = Yup.object({
       name: Yup.string().required(),
       price: Yup.number().required(),
-      category: Yup.string().required(),
+      category_id: Yup.number().required(),
     });
 
     try {
@@ -16,12 +18,12 @@ class ProductController {
     }
     
     const { filename: path } = request.file;
-    const { name, price, category } = request.body;
+    const { name, price, category_id } = request.body;
 
     const product = await Product.create({
       name,
       price,
-      category,
+      category_id,
       path,
     });
  
@@ -29,9 +31,15 @@ class ProductController {
   }
 
   async index(request, response) {
-    const products = await Product.findAll();
-
-    console.log({ userId: request.userId });
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: 'category',
+          attributes: ['id', 'name'],
+        }
+      ]
+    });
 
     return response.json(products);
   }
--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import configDatabase from '../config/database';
 
@@ -12,6 +13,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -21,6 +23,12 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo () {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburger',
+    );
   }
 }
 
