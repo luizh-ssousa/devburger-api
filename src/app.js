8ecea7be8fa9941978ca6090b3853748b32441ea
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-11 21:41:27 -0300

    username

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ class App {
     this.app.use(express.json());
     this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')),
     );
+
+    this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')),
+    );
   }
 
   routes() {
--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -5,6 +5,14 @@ class Category extends Model {
     super.init(
       {
         name: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3001/category-file/${this.path}`;
+          },
+        },
+
       },
       {
         sequelize,
